cb6090fd2ca942e243f1a96e17387147c30c6c09
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-03-02 16:57:22 -0300

    coias novas ai

---
+++ 1. variaveis.js
@@ -0,0 +1,15 @@
+// variáveis
+
+let nome = "Marta"
+let idade = 25 
+let peso = 70 
+let altura = 1.65
+
+// Testando variável.
+nome = "José"
+
+// Exibir no console
+console.log("Nome: ", nome)
+console.log("Idade: ", idade)
+console.log("Peso: ", peso)
+console.log("Altura: ", altura)
---
+++ 3. var.js
@@ -0,0 +1,7 @@
+// variáveis.
+
+// Declaração de variável em projetos 
+// antigos/legados.
+var nome = "Marta"
+
+console.log("Nome; ", nome)
